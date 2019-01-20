0622ac825cdb0d586fa45450ccbdbca92703c7fa
chunbong <cdy0266@gmail.com>
2019-01-20 19:59:15 +0900

    ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-// import express
-const express = require('express');
+// 최신 JavaScript로 변경
+import express from "express";
+
 // execute express
 const app = express();
 
 const PORT = 4000;
 
 
-function handleListening() {
-   console.log(`Listening on: http://localhost:${PORT}`);
-}
-
+// 최신 JavaScript ES6으로 변경 ( errow function )
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 // 함수는 2가지(request object, response object)를 호출
-function handleHome(req, res) {
-   res.send("welcome");
-}
+const handleHome = (req, res) => res.send("welcome");
 
-function handleProfile(req, res) {
-   res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 
 // "/"에 접근하면 handleHome 함수를 호출 
