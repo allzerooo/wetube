e79c8c0877adcddcf9b685030da241ff344ee402
chunbong <cdy0266@gmail.com>
2019-01-20 18:20:42 +0900

    Handling Routes

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleListening() {
    console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+// 함수는 2가지(request object, response object)를 호출
+function handleHome(req, res) {
+   res.send("welcome");
+}
+
+function handleProfile(req, res) {
+   res.send("You are on my profile");
+}
+
+
+// "/"에 접근하면 handleHome 함수를 호출 
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
