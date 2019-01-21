099bbcee86eaeb213a3a0028700bfa07203de1f8
chunbong <cdy0266@gmail.com>
2019-01-21 23:17:16 +0900

    Add a router

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
 
-// 최신 JavaScript ES6으로 변경 ( errow function )
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 // 함수는 2가지(request object, response object)를 호출
 const handleHome = (req, res) => res.send("welcome");
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -21,7 +19,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 // "/"에 접근하면 handleHome 함수를 호출 
-app.get("/", handleHome);
+app.use("/", userRouter);
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
