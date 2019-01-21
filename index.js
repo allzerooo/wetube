6a5b8a19852092eeed2f14c86b76ceff355eac78
chunbong <cdy0266@gmail.com>
2019-01-21 22:29:17 +0900

    Add middleware

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-// 최신 JavaScript로 변경
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-// execute express
 const app = express();
-
 const PORT = 4000;
-
 
 // 최신 JavaScript ES6으로 변경 ( errow function )
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 // 함수는 2가지(request object, response object)를 호출
 const handleHome = (req, res) => res.send("welcome");
-
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 // "/"에 접근하면 handleHome 함수를 호출 
 app.get("/", handleHome);
