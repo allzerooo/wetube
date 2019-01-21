3f507fe6886afcc7cd8fd0dfd0d1d381d7328e5e
chunbong <cdy0266@gmail.com>
2019-01-21 23:50:40 +0900

    Add routers

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 
 const app = express();
 
-// 함수는 2가지(request object, response object)를 호출
-const handleHome = (req, res) => res.send("welcome");
-const handleProfile = (req, res) => res.send("You are on my profile");
 
-
+// middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-// "/"에 접근하면 handleHome 함수를 호출 
-app.use("/", userRouter);
-app.get("/profile", handleProfile);
+
+app.user("/", globalRouter);
+app.use("/user", userRouter);
+app.user("/video", videoRouter);
+
 
 export default app;
