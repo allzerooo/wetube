753ec8ce1282e719bf12425feddd27b7f3199bc3
chunbong <cdy0266@gmail.com>
2019-02-04 22:18:57 +0900

    Create middleware to use local variables as global variables

---
+++ middlewares.js
@@ -0,0 +1,8 @@
+import routes from "./routes";
+
+// local 변수를 global 변수로 사용하도록 만들어주는 middleware를 생성
+export const localsMiddleware = (req, res, next) => {
+   res.locals.siteName = "WeTube";
+   res.locals.routes = routes;
+   next();
+};
