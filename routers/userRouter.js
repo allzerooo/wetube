8b9b1683c8e374e38fd88a5a7fef496947b2e7ee
chunbong <cdy0266@gmail.com>
2019-02-04 16:34:26 +0900

    Apply layout to all templates

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { users_Controller, user_detail_Controller, edit_profile_Controller, change_password_Controller } from "../Controllers/userController";
+import { user_detail_Controller, edit_profile_Controller, change_password_Controller } from "../Controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users_Controller);
 userRouter.get(routes.userDetail, user_detail_Controller);
 userRouter.get(routes.editProfile, edit_profile_Controller);
 userRouter.get(routes.changePassword, change_password_Controller);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { video_detail_Controller, video_upload_Controller, video_edit_Controller, videos_Controller, video_delete_Controller } from "../Controllers/videoController";
+import { video_detail_Controller, video_upload_Controller, video_edit_Controller, video_delete_Controller } from "../Controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos_Controller);
 videoRouter.get(routes.upload, video_upload_Controller);
 videoRouter.get(routes.videoDetail, video_detail_Controller);
 videoRouter.get(routes.editVdeo, video_edit_Controller);
