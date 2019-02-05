0d2e2680fadf326a5cc16d282d1328c00f8d9292
chunbong <cdy0266@gmail.com>
2019-02-05 09:20:45 +0900

    Passing variable to the template

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,9 +1,9 @@
 // for global router
-export const user_join_Controller = (req, res) => res.render("join");
-export const user_login_Controller = (req, res) => res.render("login");
-export const user_logout_Controller = (req, res) => res.render("logout");
+export const user_join_Controller = (req, res) => res.render("join", { pageTitle: "Join"});
+export const user_login_Controller = (req, res) => res.render("login", { pageTitle: "Login"});
+export const user_logout_Controller = (req, res) => res.render("logout", { pageTitle: "Logout"});
 
 // for user router
-export const user_detail_Controller = (req, res) => res.render("userDetail");
-export const edit_profile_Controller = (req, res) => res.render("editProfile");
-export const change_password_Controller = (req, res) => res.render("changePassword");
+export const user_detail_Controller = (req, res) => res.render("userDetail", { pageTitle: "User Detail"});
+export const edit_profile_Controller = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
+export const change_password_Controller = (req, res) => res.render("changePassword", { pageTitle: "Change Password"});
--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,10 +1,10 @@
 // home은 video들을 보여주기 때문에 video 컨트롤러에 위치
-export const video_home_Controller = (req, res) => res.render("home");
+export const video_home_Controller = (req, res) => res.render("home", { pageTitle: "Home"});
 
 // search는 video를 탐색하기 때문에 video 컨트롤러에 위치
-export const video_search_Controller = (req, res) => res.render("search");
+export const video_search_Controller = (req, res) => res.render("search", { pageTitle: "Search"});
 
-export const video_upload_Controller = (req, res) => res.render('upload');
-export const video_detail_Controller = (req, res) => res.render('videoDetail');
-export const video_edit_Controller = (req, res) => res.render('editVideo');
-export const video_delete_Controller = (req, res) => res.render('deleteVideo');
+export const video_upload_Controller = (req, res) => res.render("upload", { pageTitle: "Upload"});
+export const video_detail_Controller = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail"});
+export const video_edit_Controller = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
+export const video_delete_Controller = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video"});
