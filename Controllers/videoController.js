844d9228a40f9e0bb40b3dfdc45eb8593227d5e6
chunbong <cdy0266@gmail.com>
2019-02-06 12:35:09 +0900

    Add data to the template

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -2,7 +2,11 @@
 export const video_home_Controller = (req, res) => res.render("home", { pageTitle: "Home"});
 
 // search는 video를 탐색하기 때문에 video 컨트롤러에 위치
-export const video_search_Controller = (req, res) => res.render("search", { pageTitle: "Search"});
+export const video_search_Controller = (req, res) => {
+   // const searchingBy == req.query.term
+   const {query: { term: searchingBy }} = req;
+   res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+};
 
 export const video_upload_Controller = (req, res) => res.render("upload", { pageTitle: "Upload"});
 export const video_detail_Controller = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail"});
