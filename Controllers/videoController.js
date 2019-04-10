ebaac4b6a6792fb7dd47b1704f40c635c13c4196
chunbong <cdy0266@gmail.com>
2019-04-10 23:02:08 +0900

    Edit Video

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -50,5 +50,22 @@ export const video_detail_Controller = async (req, res) => {
       res.redirect(routes.home);
    }
 };
-export const video_edit_Controller = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
+export const video_getEdit_Controller = async (req, res) => {
+   const {
+      params: {id}
+   } = req;
+   try {
+      const video = await Video.findById(id);
+      res.render("editVideo", { pageName: `Edit ${video.title}`, video });
+   } catch(error) {
+      res.redirect(routes.home);
+   }
+}
+
+export const video_postEdit_Controller = (req, res) => {
+   const {
+      params: {id}
+   } = req;
+}
+
 export const video_delete_Controller = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video"});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { video_detail_Controller, video_postUpload_Controller, video_edit_Controller, video_delete_Controller, video_getUpload_Controller } from "../Controllers/videoController";
+import { video_detail_Controller, video_postUpload_Controller, video_getEdit_Controller, video_delete_Controller, video_getUpload_Controller, video_postEdit_Controller } from "../Controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -9,7 +9,10 @@ videoRouter.get(routes.upload, video_getUpload_Controller)
 videoRouter.post(routes.upload, uploadVideo, video_postUpload_Controller);
 
 videoRouter.get(routes.videoDetail(), video_detail_Controller);
-videoRouter.get(routes.editVideo, video_edit_Controller);
+
+videoRouter.get(routes.editVideo(), video_getEdit_Controller);
+videoRouter.post(routes.editVideo(), video_postEdit_Controller)
+
 videoRouter.get(routes.deleteVideo, video_delete_Controller);
 
 
