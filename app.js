b2dbf607b11974a6914d8fcf78383d8b90ce43bc
chunbong <cdy0266@gmail.com>
2019-05-01 14:00:39 +0900

    Configuring webpack for js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 
-
 const app = express();
 
 // middleware
@@ -20,6 +19,7 @@ app.set("view engine", "pug");
 
 // '/uploads/video'에 대한 routing을 위해 directory에서 file을 보내주는 middleware를 사용
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 
 // middleware
 app.use(cookieParser());
@@ -31,6 +31,5 @@ app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-
 
 export default app;
