ad867f6fedee24ee4c7f0f67ef3a47f00cf6215a
chunbong <cdy0266@gmail.com>
2019-05-03 23:40:32 +0900

    Footer and Login / Join

--- middlewares.js
+++ middlewares.js
@@ -5,13 +5,13 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 
 // local 변수를 global 변수로 사용하도록 만들어주는 middleware를 생성
 export const localsMiddleware = (req, res, next) => {
-   res.locals.siteName = "WeTube";
-   res.locals.routes = routes;
-   res.locals.user = {
-      isAuthenticated: true,
-      id: 1
-   };
-   next();
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1
+  };
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
