9c747ff52ceda84f3431785d37ca559f3ab25df7
chunbong <cdy0266@gmail.com>
2019-05-10 11:22:51 +0900

    Local Authentication with Passport

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,40 +1,52 @@
 import routes from "../routes";
+import User from "../models/User";
 
 // for global router
 export const user_getJoin_Controller = (req, res) => {
-   res.render("join", { pageTitle: "Join"});
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const user_postJoin_Controller = (req, res) => {
-   console.log(res);
-   const {
-      body: { name, email, password, verify_password }
-   } = req;
+export const user_postJoin_Controller = async (req, res) => {
+  //   console.log(res);
+  const {
+    body: { name, email, password, verify_password }
+  } = req;
 
-   if ( password !== verify_password ) {
-      res.status(400);
-      res.render("join", { pageTitle: "Join"});
-   } else {
-      // To Do : Register User
-      // To Do : Log user in
-      res.redirect(routes.home);
-   }
+  if (password !== verify_password) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User.create({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // To Do : Log user in
+    res.redirect(routes.home);
+  }
 };
 
-
-export const user_getLogin_Controller = (req, res) => res.render("login", { pageTitle: "Login"});
+export const user_getLogin_Controller = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
 export const user_postLogin_Controller = (req, res) => {
-   res.redirect(routes.home);
-}
+  res.redirect(routes.home);
+};
 
 export const user_logout_Controller = (req, res) => {
-   // To Do : Process Log Out
-   res.redirect(routes.home);
-}
-
+  // To Do : Process Log Out
+  res.redirect(routes.home);
+};
 
 // for user router
-export const user_detail_Controller = (req, res) => res.render("userDetail", { pageTitle: "User Detail"});
-export const edit_profile_Controller = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
-export const change_password_Controller = (req, res) => res.render("changePassword", { pageTitle: "Change Password"});
+export const user_detail_Controller = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const edit_profile_Controller = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const change_password_Controller = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
