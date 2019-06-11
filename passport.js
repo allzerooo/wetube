2f9204bf32828a82c777d12e62caa735c2d73f88
chunbong <cdy0266@gmail.com>
2019-06-11 21:54:52 +0900

    Add comments

--- passport.js
+++ passport.js
@@ -1,6 +1,8 @@
 import passport from "passport";
 import User from "./models/User";
 
+// passport가 사용할 strategy를 명시
+// 원하는 만큼 많은 strategy를 사용할 수 있다
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
